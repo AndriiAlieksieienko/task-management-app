1fec90f4046e15c5df27101e6304bbf16ca6f1c1
Andrii Alieksieienko <andriy.alieksieienko@gmail.com>
2026-08-05 17:15:12 +0300

    added the admin user

---
+++ src/main/java/com/andrii/taskmanagement/config/AdminInitializer.java
@@ -0,0 +1,65 @@
+package com.andrii.taskmanagement.config;
+
+import com.andrii.taskmanagement.exception.EntityNotFoundException;
+import com.andrii.taskmanagement.model.Role;
+import com.andrii.taskmanagement.model.RoleName;
+import com.andrii.taskmanagement.model.User;
+import com.andrii.taskmanagement.repository.user.RoleRepository;
+import com.andrii.taskmanagement.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class AdminInitializer implements CommandLineRunner {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${app.admin.username}")
+    private String adminUsername;
+
+    @Value("${app.admin.email}")
+    private String adminEmail;
+
+    @Value("${app.admin.password}")
+    private String adminPassword;
+
+    @Value("${app.admin.first-name}")
+    private String adminFirstName;
+
+    @Value("${app.admin.last-name}")
+    private String adminLastName;
+
+    @Override
+    public void run(String... args) {
+        if (userRepository.existsByEmail(adminEmail)
+                || userRepository.existsByUsername(adminUsername)) {
+            return;
+        }
+
+        Role role = roleRepository.findByName(RoleName.ROLE_ADMIN)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        RoleName.ROLE_ADMIN + " not found"
+                ));
+
+        User admin = new User();
+        admin.setUsername(adminUsername);
+        admin.setEmail(adminEmail);
+        admin.setPassword(passwordEncoder.encode(adminPassword));
+        admin.setFirstName(adminFirstName);
+        admin.setLastName(adminLastName);
+        admin.setRole(role);
+
+        LocalDateTime now = LocalDateTime.now();
+        admin.setCreatedAt(now);
+        admin.setUpdatedAt(now);
+
+        userRepository.save(admin);
+    }
+}
