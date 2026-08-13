2e7e3fed4ecc10c778f6c351c5f23c217cb363f6
Andrii Alieksieienko <andriy.alieksieienko@gmail.com>
2026-08-13 19:38:44 +0300

    set up Testcontainers

--- src/test/java/com/andrii/taskmanagement/TaskManagementAppApplicationTests.java
+++ src/test/java/com/andrii/taskmanagement/TaskManagementAppApplicationTests.java
@@ -2,11 +2,19 @@ package com.andrii.taskmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 @ActiveProfiles("test")
 class TaskManagementAppApplicationTests {
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0");
 
     @Test
     void contextLoads() {
